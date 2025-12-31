92918d6e9816eca3d2223498d70846126e76b771
sakitha jayasinghe <sakithajayasinghe@gmail.com>
2025-12-31 21:37:13 +0530

    Template added

--- WeatherForecast/WeatherForecastApp.swift
+++ WeatherForecast/WeatherForecastApp.swift
@@ -1,17 +1,42 @@
 //
-//  WeatherForecastApp.swift
-//  WeatherForecast
+//  WeatherDashboardTemplateApp.swift
+//  WeatherDashboardTemplate
 //
-//  Created by sakitha on 2025-12-31.
+//  Created by girish lukka on 18/10/2025.
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct WeatherForecastApp: App {
+
+    // code to set configure ViewModel and ModelContainer
+    @StateObject private var vm: MainAppViewModel
+    private let container: ModelContainer
+    init() {
+
+        //  Define schema for all models
+        let schema = Schema([Place.self, AnnotationModel.self])
+
+        //  Persistent (on-disk) configuration
+        let configuration = ModelConfiguration(isStoredInMemoryOnly: false)
+        self.container = try! ModelContainer(for: schema, configurations: [configuration])
+
+        //  Create main model context
+        let context = ModelContext(container)
+        _vm = StateObject(wrappedValue: MainAppViewModel(context: context))
+
+
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavBarView()
+                .environmentObject(vm)
+            //  Attach the same persistent container (not a new one!)
+                .modelContainer(container)
         }
     }
+
 }
